8211879af6c4578bd26261b6e5b03fc5abb8a7ef
Kevin Yun <kevin@growsurf.com>
2023-10-08 12:34:38 +0800

    fix edge-case bug

--- content.js
+++ content.js
@@ -13,21 +13,29 @@ function addLinkTitleToTweets() {
     const elementToGetStylesFrom = tweet.querySelector('div[data-testid="tweetText"] > span');
     // const fontFamily = window.getComputedStyle(elementToGetStylesFrom).getPropertyValue('font-family').split(',')[0].trim();
     const fontFamily = 'TwitterChirp';
-    const backgroundColor = window.getComputedStyle(elementToGetStylesFrom).getPropertyValue('background-color');
-    const color = window.getComputedStyle(elementToGetStylesFrom).getPropertyValue('color');
+
+    let backgroundColor;
+    let color;
+
+    if (elementToGetStylesFrom) {
+      backgroundColor = window.getComputedStyle(elementToGetStylesFrom).getPropertyValue('background-color');
+      color = window.getComputedStyle(elementToGetStylesFrom).getPropertyValue('color');
+    }
 
     // If there is a urlAndTitle, then we know it's for an image link
     if (urlAndTitle) {
       const urlAndTitleAriaLabel = urlAndTitle.getAttribute('aria-label');
-      const url = urlAndTitleAriaLabel.split(' ')[0];
-      const title = urlAndTitleAriaLabel.split(`${url} `)[1];
+      if (urlAndTitleAriaLabel) {
+        const url = urlAndTitleAriaLabel.split(' ')[0];
+        const title = urlAndTitleAriaLabel.split(`${url} `)[1];
 
-      if (url) {
-        const htmlToAppend = `<div style="padding: 0.75em 1em; margin-bottom: 30px; color: ${color}; background-color: ${backgroundColor}; font-family: '${fontFamily}'">${title}</div>`;
-        urlAndTitle.insertAdjacentHTML('beforeend', htmlToAppend);
+        if (url) {
+          const htmlToAppend = `<div style="padding: 0.75em 1em; margin-bottom: 30px; color: ${color}; background-color: ${backgroundColor}; font-family: '${fontFamily}'">${title}</div>`;
+          urlAndTitle.insertAdjacentHTML('beforeend', htmlToAppend);
 
-        // Mark this tweet as processed
-        processedTweets.add(tweet);
+          // Mark this tweet as processed
+          processedTweets.add(tweet);
+        }
       }
     }
   });
